d3ec024b9196da7e01e0ba557b196ad3b1d1958f
ShibinCo <shibinazx@gmail.com>
2018-06-09 21:44:30 +0530

    Initial Commit

---
+++ bot.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const Telegram = require('telegram-node-bot')
+const TelegramBaseController = Telegram.TelegramBaseController
+const TextCommand = Telegram.TextCommand
+const tg = new Telegram.Telegram('YOUR_TOKEN')
+
+class PingController extends TelegramBaseController {
+    /**
+     * @param {Scope} $
+     */
+    pingHandler($) {
+        $.sendMessage('pong')
+    }
+
+    get routes() {
+        return {
+            'pingCommand': 'pingHandler'
+        }
+    }
+}
+
+tg.router
+    .when(
+        new TextCommand('ping', 'pingCommand'),
+        new PingController()
+    )
